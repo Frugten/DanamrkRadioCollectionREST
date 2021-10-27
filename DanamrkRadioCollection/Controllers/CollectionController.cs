bd59ea872e4d946d16efec3216eedbc83fa06b8a
Frugten <75611171+Frugten@users.noreply.github.com>
2021-10-27 12:29:52 +0200

    Klargjort :)

---
+++ DanamrkRadioCollection/Controllers/CollectionController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using RESTbookFirst.Managers;
+using RESTbookFirst.Models;
+
+namespace DanamrkRadioCollection.Controllers
+{
+    public class CollectionController
+    {
+        [Route("api/[controller]")]
+        // the controller is available on ..../api/books
+        // [controller] means the name of the controller minus "Controller"
+        [ApiController]
+        public class BooksController : ControllerBase
+        {
+            private readonly BooksManager _manager = new BooksManager();
+
+            // GET: api/<BooksController>
+            [HttpGet]
+            public IEnumerable<Book> Get()
+            {
+                return _manager.GetAll();
+            }
+
+            // GET api/<BooksController>/5
+            [HttpGet("{id}")]
+            public Book Get(int id)
+            {
+                return _manager.GetById(id);
+            }
+
+            // POST api/<BooksController>
+            [HttpPost]
+            public Book Post([FromBody] Book value)
+            {
+                return _manager.Add(value);
+            }
+
+            // PUT api/<BooksController>/5
+            [HttpPut("{id}")]
+            public Book Put(int id, [FromBody] Book value)
+            {
+                return _manager.Update(id, value);
+            }
+
+            // DELETE api/<BooksController>/5
+            [HttpDelete("{id}")]
+            public Book Delete(int id)
+            {
+                return _manager.Delete(id);
+            }
+        }
+    }
+}
+
